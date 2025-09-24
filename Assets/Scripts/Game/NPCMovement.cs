a5ce03adc7cf0b0fa951c0c0da111be947a09895
Quincy <fayetendilla@gmail.com>
2025-09-25 01:23:53 +0800

    npc roam + spawn limit

--- Assets/Scripts/Game/NPCMovement.cs
+++ Assets/Scripts/Game/NPCMovement.cs
@@ -1,5 +1,6 @@
-using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class NPCMovement : MonoBehaviour
 {
@@ -15,13 +16,23 @@ public class NPCMovement : MonoBehaviour
 
     private List<Node> path;
 
+    private GameObject[] waypoints;
 
+    private Transform targetWaypoint;
+
+    bool isCoroutineRunning = false;
 
     private void Start()
     {
         GameObject gridObj = GameObject.FindGameObjectWithTag("Grid");
         grid = gridObj.GetComponent<WorldGrid>();
         pathfinder = gameObject.GetComponent<PathfindDijkstra>();
+
+        waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+
+        targetWaypoint = GetRandomWaypoint();
+        
+        target.transform.position = targetWaypoint.position;
     }
 
     private void Update()
@@ -32,17 +43,50 @@ public class NPCMovement : MonoBehaviour
             Node currentNode = grid.NodeFromWorldPoint(seeker.position);
             Node targetNode = grid.NodeFromWorldPoint(target.position);
 
-            if (currentNode != targetNode)
+            if (currentNode.worldPosition != targetNode.worldPosition)
             {
-                Node pathNode = path[0];    //first node in the list
+                Node pathNode = path[0];    // first node in the list
 
                 Vector3 nodePosition = pathNode.worldPosition;
                 float move = movementSpeed * Time.deltaTime;
 
                 seeker.transform.position = Vector3.MoveTowards(seeker.position, nodePosition, move);
             }
-            
+
+        }
+        else if (!isCoroutineRunning)
+        {
+            StartCoroutine(NewTarget(1f));
+        }
+    }
+
+    public Transform GetRandomWaypoint()
+    {
+        if (waypoints == null || waypoints.Length == 0)
+        {
+            Debug.LogWarning("No waypoints assigned.");
+            return null;
         }
 
+        int randomIndex = Random.Range(0, waypoints.Length);
+
+        Transform waypoint = waypoints[randomIndex].GetComponent<Transform>();
+
+        Debug.Log("Picked waypoint: " + waypoint.name);
+
+        return waypoint;
+    }
+
+    IEnumerator NewTarget(float interval)
+    {
+        isCoroutineRunning = true;
+
+        yield return new WaitForSeconds(interval);
+
+        targetWaypoint = GetRandomWaypoint();
+
+        target.transform.position = targetWaypoint.position;
+
+        isCoroutineRunning = false;
     }
 }
