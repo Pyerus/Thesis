029e25fbd0bafd624a07d72ea97806a90fc7749c
Clyyo <joeuanpcalayo@gmail.com>
2025-09-25 13:17:07 +0800

    slider animation fix
    starts from close

--- Assets/Scripts/UI/SliderMenu.cs
+++ Assets/Scripts/UI/SliderMenu.cs
@@ -1,10 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SliderMenu : MonoBehaviour
 {
     public GameObject PanelMenu;
+
+    void Start()
+    {
+        if (PanelMenu != null)
+        {
+            Animator animator = PanelMenu.GetComponent<Animator>();
+            if (animator != null)
+            {
+                animator.updateMode = AnimatorUpdateMode.UnscaledTime;
+                animator.SetBool("show", false); 
+            }
+        }
+    }
 
     public void ShowHideMenu()
     {
@@ -13,12 +24,10 @@ public class SliderMenu : MonoBehaviour
             Animator animator = PanelMenu.GetComponent<Animator>();
             if (animator != null)
             {
-                
                 animator.updateMode = AnimatorUpdateMode.UnscaledTime;
 
                 bool isOpen = animator.GetBool("show");
                 animator.SetBool("show", !isOpen);
-                
             }
             else
             {
